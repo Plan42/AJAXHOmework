3434fa65b49e09d1bb5f8ea2a35586536fd0ef16
Plan42 <110227142+Plan42@users.noreply.github.com>
2020-10-11 17:25:32 +0000

    take 3

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,4 @@ mod optim;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the g
