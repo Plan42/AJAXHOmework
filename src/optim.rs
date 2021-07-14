bbdb46af6d78c969a1bd638c1fccc70568ebe14e
Plan42 <110227142+Plan42@users.noreply.github.com>
2021-07-14 20:05:33 +0000

    GoDoc

--- src/optim.rs
+++ src/optim.rs
@@ -10,3 +10,6 @@ struct AdamOptimizer {
     m: Vec<f32>,
     v: Vec<f32>,
 }
+
+impl AdamOptimizer {
+    fn new(learning_rate: f32, beta1: f32, beta2: f32, epsilon: f32, params: &[f32]) -> AdamOptimizer {
