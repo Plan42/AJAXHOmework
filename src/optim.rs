96fe7dd410b80d585c60a359894f83827e38438a
Plan42 <110227142+Plan42@users.noreply.github.com>
2021-07-18 14:48:21 +0000

    remove log message

--- src/optim.rs
+++ src/optim.rs
@@ -15,3 +15,5 @@ impl AdamOptimizer {
     fn new(learning_rate: f32, beta1: f32, beta2: f32, epsilon: f32, params: &[f32]) -> AdamOptimizer {
         AdamOptimizer {
             learning_rate,
+            beta1,
+            beta2,
