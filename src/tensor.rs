617a0dddfa9bc853b79ee78c13dc9f591c71bb61
Plan42 <110227142+Plan42@users.noreply.github.com>
2021-09-29 15:32:08 +0000

    Undo last commit.

--- src/tensor.rs
+++ src/tensor.rs
@@ -4,3 +4,4 @@ use std::ops::{Add, Mul};
 /// A multi-dimensional array of floating-point values.
 struct Tensor {
     /// The values of the tensor as a vector of floating-point numbers.
+    data: Vec<f32>,
