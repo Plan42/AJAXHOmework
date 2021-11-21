6fe0dca4bc12baff7141a76b7830c3f16725cd93
Plan42 <110227142+Plan42@users.noreply.github.com>
2021-11-21 08:43:47 +0000

    Fixed variable names

--- src/tensor.rs
+++ src/tensor.rs
@@ -47,3 +47,7 @@ impl Tensor {
     fn shape(&self) -> &Vec<usize> {
         &self.shape
     }
+
+    /// Returns the rank of the tensor, i.e. the number of dimensions.
+    fn rank(&self) -> usize {
+        self.shape.len()
