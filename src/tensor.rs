d16da25bf065b27b1cd36fe7c70d7968fedc9946
Plan42 <110227142+Plan42@users.noreply.github.com>
2022-01-25 19:32:48 +0000

    tried F2.cpp to 'F'

--- src/tensor.rs
+++ src/tensor.rs
@@ -114,3 +114,5 @@ mod tests {
         assert_eq!(t.shape(), &vec![2, 3]);
         t.reshape(vec![3, 2]);
         assert_eq!(t.size(), 6);
+        assert_eq!(t.rank(), 2);
+        assert_eq!(t.shape(), &vec![3, 2]);
