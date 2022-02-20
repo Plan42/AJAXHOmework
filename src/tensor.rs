50f9a66ef932d949ce13640b9a41bee3a1302255
Plan42 <110227142+Plan42@users.noreply.github.com>
2022-02-20 18:31:39 +0000

    more tags

--- src/tensor.rs
+++ src/tensor.rs
@@ -133,3 +133,5 @@ mod tests {
         use super::Tensor;
         let t1 = Tensor::new(vec![1.0, 2.0, 3.0, 4.0], vec![2, 2]);
         let t2 = Tensor::new(vec![5.0, 6.0, 7.0, 8.0], vec![2, 2]);
+        let t3 = t1 * t2;
+        assert_eq!(t3.data, vec![5.0, 12.0, 21.0, 32.0]);
