65f28fb11eba8b0e348c6625c7611191f4b6595c
Plan42 <110227142+Plan42@users.noreply.github.com>
2023-01-02 09:54:29 +0000

    Update generated Bach.java

--- src/utils.rs
+++ src/utils.rs
@@ -6,4 +6,4 @@ pub fn set_panic_hook() {
     // For more details see
     // https://github.com/rustwasm/console_error_panic_hook#readme
     #[cfg(feature = "console_error_panic_hook")]
-    consol
+    console_e
