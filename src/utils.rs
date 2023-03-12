15babc5b65e3581c62c99c0f0d077758809054c5
Plan42 <110227142+Plan42@users.noreply.github.com>
2023-03-12 05:36:03 +0000

    Fixed review issues

--- src/utils.rs
+++ src/utils.rs
@@ -7,3 +7,4 @@ pub fn set_panic_hook() {
     // https://github.com/rustwasm/console_error_panic_hook#readme
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
+}
